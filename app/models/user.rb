ba5633e1e0748f895c43d200306c25cf3208a76b
Chelsea <ckochan@kinesissurvey.com>
2016-04-27 18:56:28 +0000

    Add uniqueness validation for User email.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+	
+	before_save { self.email = email.downcase }
+	
 	validates :name,  presence: true, length: { maximum: 50 } 
 	
 	VALID_EMAIL_REGEX = /[\w+\-.]+@[a-z\d\-.]+\.[a-z]+/i
